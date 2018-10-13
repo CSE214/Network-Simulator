cb48352bb46414b58590041bc95377d2df96512e
Sayan Sivakumaran <sivakusayan@gmail.com>
2018-10-13 08:03:05 -0400

    First commit

---
+++ Packet.java
@@ -0,0 +1,95 @@
+package networksimulator;
+
+/**
+ * The <code>Packet</code> class represents a packet that will be sent 
+ * through the network.
+ * 
+ * @author Sayan Sivakumaran e-mail: sayan.sivakumaran@stonybrook.edu Stony
+ *         Brook ID: 110261379
+ **/
+public class Packet {
+	private static int packetCount = 0; // Number of packets
+	private int id; // Packet identifier
+	private int packetSize; // Size of packet
+	private int timeArrive; // Time of creation
+	private int timeToDest; // Time to destination
+	/**
+	 * @return 
+	 *	The packetCount
+	 */
+	public static int getPacketCount() {
+		return packetCount;
+	}
+	/**
+	 * @return 
+	 *	The packetCount
+	 */
+	public static void setPacketCount(int newPacketCount) {
+		packetCount = newPacketCount;
+	}
+	/**
+	 * @return 
+	 *	The id of this instance
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id 
+	 * 	The new id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return 
+	 *	The packetSize of this instance
+	 */
+	public int getPacketSize() {
+		return packetSize;
+	}
+	/**
+	 * @param packetSize 
+	 * 	The new packetSize to set
+	 */
+	public void setPacketSize(int packetSize) {
+		this.packetSize = packetSize;
+	}
+	/**
+	 * @return 
+	 *	The timeArrive of this instance
+	 */
+	public int getTimeArrive() {
+		return timeArrive;
+	}
+	/**
+	 * @param timeArrive 
+	 * 	The new timeArrive to set
+	 */
+	public void setTimeArrive(int timeArrive) {
+		this.timeArrive = timeArrive;
+	}
+	/**
+	 * @return 
+	 *	The timeToDest of this instance
+	 */
+	public int getTimeToDest() {
+		return timeToDest;
+	}
+	/**
+	 * @param timeToDest 
+	 * 	The new timeToDest to set
+	 */
+	public void setTimeToDest(int timeToDest) {
+		this.timeToDest = timeToDest;
+	}
+	
+	/**
+	 * Returns an instance of Packet
+	 */
+	public Packet(int minSize, int maxSize) {
+		this.id = packetCount;
+		packetCount += 1;
+		this.packetSize = (int) (Math.random()*(maxSize - minSize) + minSize);
+	}
+}
---
+++ Simulator.java
@@ -0,0 +1,14 @@
+package networksimulator;
+
+
+/**
+ * The <code>Simulator</code> class allows the user to run the simulation.
+ * 
+ * @author Sayan Sivakumaran e-mail: sayan.sivakumaran@stonybrook.edu Stony
+ *         Brook ID: 110261379
+ **/
+public class Simulator {
+	public static void main (String[] args) {
+
+	}
+}
